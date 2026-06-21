2319d73b25c52dd54548d428fb8dfc7f29133249
N0ks1_ <artemmatenco2007@gmail.com>
2026-06-21 04:35:11 +0300

    123 cors origin fix

--- services/broadcast/src/server.ts
+++ services/broadcast/src/server.ts
@@ -14,7 +14,7 @@ dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-const corsOrigin = process.env.CORS_ORIGIN || "*";
+const corsOrigin = process.env.CORS_ALLOWED_ORIGINS;
 
 app.use(
   cors({
